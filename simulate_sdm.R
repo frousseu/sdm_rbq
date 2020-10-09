0d40458006af52f6a6c5e2309d9515b3f3509b87
frousseu <francois.rousseu@usherbrooke.ca>
2020-10-09 16:48:14 -0400

    ...

--- simulate_sdm.R
+++ simulate_sdm.R
@@ -25,28 +25,28 @@ kappa=sqrt(8*nu)/rangex
 b0=-6
 b1=0.002
 b2=b1
-sigma=sqrt(0.00000001)
+sigma=sqrt(0.00000000001)
 #exp(b0) * diff(range(o$x)) * diff(range(o$y))
 
 #set.seed(1234) # 123 avec -17 aberrant
-p<-rLGCP("matern",mu=function(x,y){b0+b1*x+b2*y},var=sigma^2,scale=1/kappa,nu=nu,win=o,dimyx=c(300,300))
+pp<-rLGCP("matern",mu=function(x,y){b0+b1*x+b2*y},var=sigma^2,scale=1/kappa,nu=nu,win=o,dimyx=c(300,300))
 cutoff<-100000
-if(p$n>cutoff){
-  pn<-p$n
-  rm(p)
+if(pp$n>cutoff){
+  pn<-pp$n
+  rm(pp)
   stop(paste("Too many points generated",pn,">",cutoff))
 }
-plot(p,axes=FALSE);p
+plot(pp,axes=FALSE);pp
 
-xy <- cbind(p$x,p$y)
+xy <- cbind(pp$x,pp$y)
 n <- nrow(xy)
-Lam <- attr(p, 'Lambda')
+Lam <- attr(pp, 'Lambda')
 summary(as.vector(rf.s <- log(Lam$v)))
 par(mfrow=c(1,2),oma=c(0,4,0,4))
 image.plot(list(x=Lam$xcol, y=Lam$yrow, z=t(exp(rf.s))), main='log-Lambda',asp=1,col=colo)
 points(xy,pch=16,col=gray(0,0.15),cex=0.6)
 
-p<-st_as_sf(p)
+p<-st_as_sf(pp)
 st_crs(p)<-prj#32617
 #p<-st_transform(p,prj)
 occ<-p[p$label!="window",]
@@ -57,10 +57,7 @@ rx<-setValues(r,coordinates(r)[,1])
 ry<-setValues(r,coordinates(r)[,2])
 r<-stack(rx,ry)
 
-model<-RMexp(var=1.5,scale=25)+RMnugget(var=0)+RMtrend(mean=0)
-#x.seq<-seq(o$xrange[1],o$xrange[2],length=300) 
-#y.seq<-seq(o$yrange[1],o$yrange[2],length=300)
-
+model<-RMexp(var=2,scale=25)+RMnugget(var=0)+RMtrend(mean=0)
 x.seq<-seq(extent(r)[1],extent(r)[2],length=300) 
 y.seq<-seq(extent(r)[3],extent(r)[4],length=300)
 sims<-RFsimulate(model,x=x.seq,y=y.seq)
@@ -70,7 +67,7 @@ eff<-round(resample(eff,r,method="ngb"))
 plot(eff)
 eff<-stack(eff,setValues(eff,1:ncell(eff)))
 le<-values(log(eff[[1]]))
-le<-ifelse(is.infinite(le),-1,le)           
+le<-ifelse(is.infinite(le),0,le)           
 r<-stack(r,eff[[1]],setValues(eff[[1]],le))
 names(r)<-c("xx","yy","effort","logeffort")
 
@@ -100,27 +97,29 @@ pedge<-0.04
 edge<-min(c(diff(bbox(region)[0.2,])*pedge,diff(bbox(region)[2,])*pedge))
 edge
 
-bpriors<-list(prec=list(default=1/(10)^2,latitude=1/(10)^2,latitude2=1/(10)^2,Intercept=1/(100)^2,sbias=1/(100)^2,effort=1/(100)^2,logeffort=1/(100)^2),mean=list(default=0,Intercept=0,latitude=0,latitude2=0,sbias=0,effort=0,logeffort=0))
+
 
 Mesh<-inla.mesh.2d(loc.domain = as(extent(region),"SpatialPoints"),max.edge = c(edge,edge*1.5),min.angle = 21,cutoff = edge/2,offset = c(edge,edge*2),crs = crs(region))
 explana<-explanaMesh(sPoly=region,mesh=Mesh,X=r)
 weight<-ppWeight(sPoly=region, mesh=Mesh)
 
-m1<-ppSpace(y ~ xx+yy+effort, sPoints = occ_thinsp,
+
+bpriors1<-list(prec=list(default=1/(10)^2,Intercept=1/(100)^2,effort=1/(10)^2,logeffort=1/(100.00000001)^2),mean=list(default=0,Intercept=0,effort=0,logeffort=0))
+m1<-ppSpace(y ~ xx+yy+logeffort, sPoints = occ_thinsp,
             explanaMesh = explana,
             ppWeight = weight,
             prior.range = c(20000,NA),
-            prior.sigma = c(0.00001,NA),
+            prior.sigma = c(0.0001,NA),
             num.threads = 7,
             many = TRUE,
             control.inla = list(int.strategy = "eb"),
-            bias = "effort",
-            control.fixed = bpriors#,#,#,
-            #orthoCons = TRUE
+            bias = "logeffort",
+            control.fixed = bpriors1,
+            orthoCons = FALSE
 )
 
-
-m2<-ppSpace(y ~ xx+yy+offset(logeffort), sPoints = occ_thinsp,
+bpriors2<-list(prec=list(default=1/(10)^2,Intercept=1/(100)^2,effort=1/(10)^2,logeffort=1/(0.00000001)^2),mean=list(default=0,Intercept=0,effort=0,logeffort=1))
+m2<-ppSpace(y ~ xx+yy+logeffort, sPoints = occ_thinsp,
              explanaMesh = explana,
              ppWeight = weight,
              prior.range = c(200,0.5),
@@ -128,9 +127,9 @@ m2<-ppSpace(y ~ xx+yy+offset(logeffort), sPoints = occ_thinsp,
              num.threads = 7,
              many = TRUE,
              control.inla = list(int.strategy = "eb"),
-             bias = NULL,
-             control.fixed = bpriors,
-             orthoCons = TRUE
+             bias = "logeffort",
+             control.fixed = bpriors2,
+             orthoCons = FALSE
 )
 
 
@@ -140,19 +139,19 @@ xlim<-bbox(region)[1,]
 ylim<-bbox(region)[2,]
 image.plot(list(x=Lam$xcol, y=Lam$yrow, z=t(exp(rf.s))), main=paste0('Simulated intensity - ',nrow(occ)," obs",sep=" "),asp=1,col=colo)
 #points(xy,pch=1,col=gray(0,0.05),cex=0.6)
-grid(10,6,col=gray(0,0.25),lty=3)
+grid(10,6,col=gray(0,0.15),lty=3)
 plot(eff[[1]],xlim=xlim,ylim=ylim,main="Effort (en entier, beaucoup de 0)",col=colo)
-grid(10,6,col=gray(0,0.25),lty=3)
+grid(10,6,col=gray(0,0.15),lty=3)
 mapMean<-mapSpace(m[[1]],dims=dim(r)[1:2],type="mean")
 mapMean <- mask(mapMean,region) #spacePoly
 plot(mapMean, col = colo, axes = TRUE, box = FALSE, main = paste0(names(m)[1]," - ",nrow(occ_thin)," obs",sep=" "),xlim=xlim,ylim=ylim,legend.shrink=0.9,legend.width=1.5)
-points(occ_thinsp,pch=1,col=gray(0,0.25),cex=0.6)
-grid(10,6,col=gray(0,0.25),lty=3)
-mapMean<-mapSpace(m[[2]],dims=dim(r)[1:2],type="space")
+#points(occ_thinsp,pch=1,col=gray(0,0.25),cex=0.6)
+grid(10,6,col=gray(0,0.15),lty=3)
+mapMean<-mapSpace(m[[2]],dims=dim(r)[1:2],type="mean")
 mapMean <- mask(mapMean,region) #spacePoly
 plot(mapMean, col = colo, axes = TRUE, box = FALSE, main = paste0(names(m)[2]," - ",nrow(occ_thin)," obs",sep=" "),xlim=xlim,ylim=ylim,legend.shrink=0.9,legend.width=1.5)
-points(occ_thinsp,pch=1,col=gray(0,0.25),cex=0.6)
-grid(10,6,col=gray(0,0.25),lty=3)
+#points(occ_thinsp,pch=1,col=gray(0,0.25),cex=0.6)
+grid(10,6,col=gray(0,0.15),lty=3)
 lapply(m,function(i){summary(i)$coef})
 print(seed)
 
@@ -169,7 +168,7 @@ rr<-rasterize(occ_thin[,1],r[[1]],fun="count",background=0)
 
 temp<-data.frame(eff=values(eff[[1]]),logeff=log(values(eff[[1]])),cou=values(rr)[,1],xx=coordinates(rr)[,1],yy=coordinates(rr)[,2])
 temp2<-temp[temp$eff>0,]
-fit<-glm(cou~xx+yy+logeff,data=temp2,family="poisson")
+fit<-glm(cou~xx+yy+effort,data=temp2,family="poisson")
 summary(fit)
 newdat<-temp
 newdat$eff<-max(temp$eff)
@@ -185,7 +184,7 @@ lines(newdat$logeff,p)
 
 temp<-data.frame(eff=values(eff[[1]]),logeff=log(values(eff[[1]])),cou=values(rr)[,1],xx=coordinates(rr)[,1],yy=coordinates(rr)[,2])
 temp2<-temp[temp$eff>0,]
-fit<-glm(cou~xx+yy+log(eff),data=temp2,family="poisson")
+fit<-glm(cou~xx+yy+logeff,data=temp2,family="poisson")
 summary(fit)
 #visreg(fit,"logeff")
 #visreg(fit,"logeff",scale="response")
