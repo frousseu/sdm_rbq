0334010c3ab3a6a6e33e327363bcf11933f19392
frousseu <francoisrousseu@hotmail.com>
2020-10-29 22:01:19 -0400

    ...

--- simulate_sdm.R
+++ simulate_sdm.R
@@ -21,16 +21,16 @@ win<-c(0,1000,0,1000)
 o<-owin(win[1:2],win[3:4])
 
 nu=2
-rangex=200
+rangex=1000
 kappa=sqrt(8*nu)/rangex
 b0=-5
 b1=0.002
 b2=b1
-sigma=sqrt(0.00000001)
+sigma=sqrt(1.00000001)
 
 
 pp<-rLGCP("matern",mu=function(x,y){b0+b1*x+b2*y},var=sigma^2,scale=1/kappa,nu=nu,win=o,dimyx=c(300,300))
-cutoff<-180000
+cutoff<-300000
 if(pp$n>cutoff){
   pn<-pp$n
   rm(pp)
@@ -45,9 +45,9 @@ summary(as.vector(rf.s <- log(Lam$v)))
 par(mfrow=c(3,5),oma=c(0,4,0,4))
 #image.plot(list(x=Lam$xcol, y=Lam$yrow, z=t(exp(rf.s))), main='log-Lambda',asp=1,col=colo,axes=FALSE)
 surface<-raster(as.im(list(x=Lam$xcol, y=Lam$yrow, z=t(exp(rf.s)))))
-plot(surface,axes=FALSE,box=FALSE,col=colo,legend.shrink=0.9,legend.width=2)
-points(xy,pch=16,col=gray(0,0.01),cex=1)
-mtext(side=3,line=-1,outer=FALSE,cex=0.6,text=paste("Simulated intensity",nrow(xy),"obs"))
+plot(surface,axes=FALSE,box=FALSE,col=colo,legend.shrink=0.9,legend.width=4)
+#points(xy,pch=16,col=gray(0,0.01),cex=1)
+mtext(side=3,line=-2,outer=FALSE,cex=0.6,text=paste("Simulated intensity",nrow(xy),"obs"))
 
 p<-st_as_sf(pp)
 st_crs(p)<-prj
@@ -59,7 +59,7 @@ rx<-setValues(r,coordinates(r)[,1])
 ry<-setValues(r,coordinates(r)[,2])
 r<-stack(rx,ry)
 
-model<-RMexp(var=2.00000002,scale=25)+RMnugget(var=0)+RMtrend(mean=-1)
+model<-RMexp(var=2,scale=25)+RMnugget(var=0)+RMtrend(mean=-0.5)
 x.seq<-seq(extent(r)[1],extent(r)[2],length=300) 
 y.seq<-seq(extent(r)[3],extent(r)[4],length=300)
 sims<-RFsimulate(model,x=x.seq,y=y.seq)
@@ -68,16 +68,16 @@ modsims<-rsims
 modsims<-setValues(modsims,exp(-4*rescale(-1*coordinates(modsims)[,1]-1*coordinates(modsims)[,2],1:0)))
 rsims<-rsims
 #par(mfrow=c(1,3))
-plot(modsims,axes=FALSE,box=FALSE,zlim=c(0,1),legend.shrink=0.9,legend.width=2,col=colorRampPalette(c("white","black"))(200))
-mtext(side=3,line=-1,outer=FALSE,cex=0.6,text="Proportional effort gradient")
+plot(modsims,axes=FALSE,box=FALSE,zlim=c(0,1),legend.shrink=0.9,legend.width=4,col=colorRampPalette(c("white","black"))(200))
+mtext(side=3,line=-2,outer=FALSE,cex=0.6,text="Proportional effort gradient")
 eff<-exp(rsims)
 #plot(eff)
 eff<-exp(rsims)*modsims
 eff<-round(resample(eff,r,method="ngb"))
 eff2<-eff
 eff2[eff2==0]<-NA
-plot(eff2,axes=FALSE,box=FALSE,legend.shrink=0.9,legend.width=2,col=colorRampPalette(c("grey90","tomato","darkred","black"))(200))
-mtext(side=3,line=-1,outer=FALSE,cex=0.6,text="Realized effort")
+plot(eff2,axes=FALSE,box=FALSE,legend.shrink=0.9,legend.width=4,col=colorRampPalette(c("grey90","tomato","darkred","black"))(200))
+mtext(side=3,line=-2,outer=FALSE,cex=0.6,text="Realized effort")
 #par(mfrow=c(1,1))
 eff<-stack(eff,setValues(eff,1:ncell(eff)))
 le<-values(log(eff[[1]]))
@@ -106,14 +106,14 @@ occ_thin<-occ[unlist(l),] # not 100% sure order is maintained
 occ_thin<-st_crop(occ_thin,setNames(win[c(1,3,2,4)],c("xmin","ymin","xmax","ymax")))
 nrow(occ_thin)
 occ_thinsp<-as(occ_thin,"Spatial")
-plot(eff[[1]],col="white",box=FALSE,axes=FALSE,legend=TRUE,legend.shrink=0.9,legend.width=2)
+plot(eff[[1]],col="white",box=FALSE,axes=FALSE,legend=TRUE,legend.shrink=0.9,legend.width=4)
 plot(occ_thinsp,pch=16,col=gray(0,0.15),add=TRUE)
-mtext(side=3,line=-1,outer=FALSE,cex=0.6,text=paste("Thinned occurences",nrow(occ_thin),"obs"))
+mtext(side=3,line=-2,outer=FALSE,cex=0.6,text=paste("Thinned occurences",nrow(occ_thin),"obs"))
 
 region<-as(extent(win),"SpatialPolygons")
 proj4string(region)<-prj
 
-pedge<-0.04
+pedge<-0.035
 edge<-min(c(diff(bbox(region)[0.2,])*pedge,diff(bbox(region)[2,])*pedge))
 
 
@@ -151,8 +151,8 @@ m<-list(m=m)
 #grid(10,6,col=gray(0,0.25),lty=3)
 mapMean<-mapSpace(m[[1]],dims=dim(r)[1:2],type="mean")
 mapMean <- mask(mapMean,region) #spacePoly
-plot(mapMean, col = colo, axes = FALSE, box = FALSE,xlim=xlim,ylim=ylim,legend.shrink=0.9,legend.width=2)
-mtext(side=3,line=-1,outer=FALSE,cex=0.6,text="Predicted surface")
+plot(mapMean, col = colo, axes = FALSE, box = FALSE,xlim=xlim,ylim=ylim,legend.shrink=0.9,legend.width=4)
+mtext(side=3,line=-2,outer=FALSE,cex=0.6,text="Predicted surface")
 #points(occ_thinsp,pch=1,col=gray(0,0.25),cex=0.6)
 #grid(10,6,col=gray(0,0.25),lty=3)
 lapply(m,function(i){summary(i)$coef})
